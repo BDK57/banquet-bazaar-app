c06c8e4641c39f385859a0e53a225619f3a645a5
Hamza Ali <hamzu792@gmail.com>
2023-12-01 01:46:39 +0500

    active/in-active status done

--- src/models/HallMode.js
+++ src/models/HallMode.js
@@ -1,35 +1,51 @@
-import mongoose from 'mongoose'
-import { Schema } from 'mongoose'
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+import moment from "moment/moment";
+
+
+
 const HallSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        unique:true
+  name: {
+    type: String,
+    unique: true,
+  },
+  Description: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  vendorid: {
+    type: Schema.Types.ObjectId,
+    ref: "vendor",
+  },
+  capacity: {
+    type: Number,
+  },
+  Price: {
+    type: Number,
+  },
+  coverimage: {
+    type: String,
+  },
+  galleryImage: [
+    {
+      type: String,
     },
-    Description: {
-        type: String,
-    },
-    location:{
-        type:String
-    },
-    vendorid:{
-        type: Schema.Types.ObjectId, 
-        ref: 'vendor' 
-    },
-    capacity:{
-        type:Number,
-    },
-    Price:{
-        type:Number,
-    },
-    coverimage:{
-        type:String,
-    },
-    galleryImage:[{
-        type:String,
-    }]
+  ],
+  activeStatus: {
+    type: Boolean,
+    default: true,
+  },
+  createdTime: {
+    type: Date,
+    default: () => moment(),
+  },
+  updatedTime: {
+    type: Date,
+    default: ()=>moment(),
+  },
+});
 
-})
-
-
-const Hall = mongoose.models.Halls || mongoose.model("Halls", HallSchema)
-export default Hall
+const Hall = mongoose.models.Halls || mongoose.model("Halls", HallSchema);
+export default Hall;
--- src/models/VendorModel.js
+++ src/models/VendorModel.js
@@ -1,24 +1,39 @@
-import mongoose from 'mongoose'
-import { Schema } from 'mongoose'
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+import moment from "moment";
+
+
+
 const VendorSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: [true, "Please enter your username"],
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    userid:{
-        type: Schema.Types.ObjectId, 
-        ref: 'users' 
-    },
-    phone:{
-        type:Number
-    }
-})
+  username: {
+    type: String,
+    required: [true, "Please enter your username"],
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  userid: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+  },
+  phone: {
+    type: Number,
+  },
+  activeStatus: {
+    type: Boolean,
+    default: true,
+  },
+  createdTime: {
+    type: Date,
+    default: () => moment(),
+  },
+  updatedTime: {
+    type: Date,
+    default: () => moment(),
+  },
+});
 
-
-const Vendor = mongoose.models.vendor || mongoose.model("vendor", VendorSchema)
-export default Vendor
+const Vendor = mongoose.models.vendor || mongoose.model("vendor", VendorSchema);
+export default Vendor;
